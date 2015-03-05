56b169efcbd4054268154afa4a0e58ad37371bc9
tobitege <tobiasteschner@googlemail.com>
2015-03-05 16:55:22 +0100

    Derp

---
+++ BoardConfig.mk
@@ -0,0 +1,84 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from MSM8974 common
+-include device/oppo/msm8974-common/BoardConfigCommon.mk
+
+#tobitege:
+#BUILD_ENFORCE_SELINUX :=1
+BLISS_BUILD_BLOCK := 1
+TARGET_DISABLE_ARM_PIE :=true
+TARGET_TC_ROM :=4.8-sm
+TARGET_TC_KERNEL :=4.9-sm
+BLISSIFY :=true
+BLISS_O3 :=true
+BLISS_GRAPHITE :=false
+BLISS_STRICT :=true
+BLISS_KRAIT :=true
+
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
+#TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
+
+# FOR SaberMod newest 4.9.3 repo installed!!!
+# Inline kernel building
+#KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+#ifndef TARGET_TC_KERNEL
+#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/linux-android-4.9/bin
+#else
+#KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/linux-android-$(TARGET_TC_KERNEL)/bin
+#endif
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oneplus/bacon/bluetooth
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+COMMON_GLOBAL_CFLAGS += -DOPPO_CAMERA_HARDWARE
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 16777216
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 536870912
+BOARD_PERSISTIMAGE_PARTITION_SIZE  := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 1388314624
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13271448576
+BOARD_USERDATAEXTRAIMAGE_PARTITION_SIZE := 59914792960
+BOARD_USERDATAEXTRAIMAGE_PARTITION_NAME := 64G
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/oneplus/bacon/rootdir/etc/fstab.bacon
+
+TARGET_OTA_ASSERT_DEVICE := bacon,A0001
+
+TARGET_INIT_VENDOR_LIB := libinit_bacon
+
+TARGET_WCNSS_MAC_PREFIX := e8bba8
+
+# Workaround for factory issue
+BOARD_VOLD_CRYPTFS_MIGRATE := true
+
+BOARD_NFC_CHIPSET := pn547
+
+AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
+AUDIO_FEATURE_ENABLED_LOW_LATENCY_CAPTURE := true
+
+# inherit from the proprietary version
+-include vendor/oneplus/bacon/BoardConfigVendor.mk
---
+++ bacon.mk
@@ -0,0 +1,93 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay vendor/extra/overlays/phone-1080p
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Haters gonna hate..
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    libinit_bacon \
+    fstab.bacon \
+    init.bacon.rc \
+    init.qcom.usb.rc \
+    ueventd.bacon.rc
+
+# Audio
+PRODUCT_COPY_FILES += \
+<<<<<<< HEAD
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+=======
+>>>>>>> 428ea52f269c301f9d61b8d453288905651fcfe5
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# NFC packages
+PRODUCT_PACKAGES += \
+    NfcNci \
+    Tag \
+    nfc_nci.pn54x.default \
+    com.android.nfc_extras
+
+# NFC access control + feature files + configuration
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
+
+# System properties
+PRODUCT_PROPERTY_OVERRIDES += \
+<<<<<<< HEAD
+    ro.telephony.default_network=9
+=======
+    ro.telephony.default_network=20
+>>>>>>> 428ea52f269c301f9d61b8d453288905651fcfe5
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# call dalvik heap config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+<<<<<<< HEAD
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+
+# call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+=======
+
+# call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+>>>>>>> 428ea52f269c301f9d61b8d453288905651fcfe5
+
+# call the proprietary setup
+$(call inherit-product-if-exists, vendor/oneplus/bacon/bacon-vendor.mk)
+
+ifneq ($(QCPATH),)
+$(call inherit-product-if-exists, $(QCPATH)/prebuilt_HY11/target/product/msm8974/prebuilt.mk)
+endif
+
+# Inherit from msm8974-common
+$(call inherit-product, device/oppo/msm8974-common/msm8974.mk)
